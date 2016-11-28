64185a9fc2c8a69e1d0f8698ce9f2e3b7d1b034e
Jacob MacDonald <jakemac@google.com>
2016-11-28 15:45:15 -0800

    add helper which can instantiate transformers defined in a library by uri

---
+++ test/transformers_test.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'package:barback/barback.dart';
+import 'package:test/test.dart';
+
+import 'package:bazel/src/bazelify/transformers.dart';
+
+import 'projects/simple_with_transformer/lib/transformer.dart';
+
+void main() {
+  test('can instantiate transformers from imported libraries', () {
+    var mode = new BarbackMode("debug");
+    var config = {'hello': 'world'};
+    var libraryUri = Platform.script.resolveUri(
+        new Uri.file('projects/simple_with_transformer/lib/transformer.dart'));
+    var transformers = createTransformersInLibrary(libraryUri, config, mode);
+    expect(transformers.length, 1);
+    expect(transformers.first is MyTransformer, isTrue);
+    var myTransformer = transformers.first as MyTransformer;
+    expect(myTransformer.settings.configuration, config);
+    expect(myTransformer.settings.mode, mode);
+  });
+}
