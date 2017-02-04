3b21cd992322ca4467cc893aa39c6b27c8fb1dac
Nate Bosch <nbosch1@gmail.com>
2017-02-03 16:18:21 -0800

    Prepare for release as dazel (#111)
    - Bump stable tag for rules_dart
      Latest rules dart supports the renamed _bazel_codegen and 'pub_'
      prefixed repository names.
    - Bump version and changelog for release

--- dazel/CHANGELOG.md
+++ dazel/CHANGELOG.md
@@ -1,5 +1,10 @@
-## 0.2.3
+## 0.3.0
 
+### Package rename
+
+* Previously this package was released as `bazel`. It's now called `dazel`
+
+### New features
 * Add `dazel build` command, which can build a web app and create a deployable
   directory for it (using dart2js), similar to `pub build`.
     * Has a single positional argument which should be the path to the html file
--- dazel/lib/src/bazelify/initialize.dart
+++ dazel/lib/src/bazelify/initialize.dart
@@ -139,7 +139,7 @@ class InitCommand extends Command {
 /// Where to retrieve the `rules_dart`.
 abstract class DartRulesSource {
   /// The default version of [DartRulesSource] if not otherwise specified.
-  static const DartRulesSource stable = const DartRulesSource.tag('v0.3.1');
+  static const DartRulesSource stable = const DartRulesSource.tag('v0.3.2');
 
   /// Use a git [commit].
   const factory DartRulesSource.commit(String commit) = _GitCommitRulesSource;
